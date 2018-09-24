c0f627c1644394e8e08b95cfc0fca2b79221820d
thomasqueirozb <38295417+thomasqueirozb@users.noreply.github.com>
2018-09-24 18:50:06 -0300

    Fix 3

--- commit.py
+++ commit.py
@@ -3,11 +3,17 @@ from os import getcwd
 
 path = getcwd()
 call(f'cd {path}', shell = True)
+print(f'>>> cd {path}')
 
 call('git add *')
+print('>>> git add *')
+
 msg = input("Commit message: ")
 if msg == '':
     msg = 'Auto'
 if msg.lower() not in ['exit', 'quit', 'q']:
     call(f'git commit -m "{msg}"')
+    print(f'>>> git commit -m {msg}')
+
     call('git push')
+    print('>>> git push')
