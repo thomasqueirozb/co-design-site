da61d5a6051bceee99dc731b95c42ccd963af399
thomasqueirozb <38295417+thomasqueirozb@users.noreply.github.com>
2018-09-24 18:48:14 -0300

    Fix 2

--- commit.py
+++ commit.py
@@ -1,13 +1,13 @@
 from subprocess import call
 from os import getcwd
 
-path=getcwd()
-call(f'cd {path}',shell=True)
+path = getcwd()
+call(f'cd {path}', shell = True)
 
 call('git add *')
-msg=input("Commit message: ")
-if msg=='':
-    msg='Auto'
-if msg.lower() not in ['exit','quit','q']:
+msg = input("Commit message: ")
+if msg == '':
+    msg = 'Auto'
+if msg.lower() not in ['exit', 'quit', 'q']:
     call(f'git commit -m "{msg}"')
     call('git push')
